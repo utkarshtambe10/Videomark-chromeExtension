53b72f22e226e010cc455450f1437ac801612c79
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-10-26 22:11:42 +0530

    coding the logic of chrome-extension

---
+++ contentScript.js
@@ -0,0 +1,59 @@
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks = [];
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        currentVideoBookmarks = await fetchBookmarks();
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark the current timestamp of the video";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+    }
+    const addNewBookmarkEventHandler = async () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at" + getTime(currentTime)
+        };
+        currentVideoBookmarks = await fetchBookmarks();
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+        })
+    }
+
+    //hack for settling edge-cases
+    let trail = "&ytExt=ON";
+    if (!window.location.href.includes(trail) && !window.location.href.includes("ab_channel") && window.location.href.includes("youtube.com/watch")) {
+        window.location.href += trail;
+    }
+
+})();
+
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(t);
+
+    return date.toISOString().substr(11, 8);
+};
